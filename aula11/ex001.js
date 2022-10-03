8fd8a3d64ac525f76fe2ec5d4f189dd5aee1c29b
Felipe Cristian Ehmke <ehmke1995@gmail.com>
2022-10-02 23:10:17 -0300

    Create ex001.js

---
+++ aula11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 81;
+console.log(`A velocidade do seu carro é: ${vel}km.`)
+if (vel > 80) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre com cinto de segurança!.`)
