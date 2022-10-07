2f19cc1dd860d800d08853f6b649aaf2a03e4bc5
Felipe Cristian Ehmke <ehmke1995@gmail.com>
2022-10-06 23:55:21 -0300

    update script.js

--- aula12/ex005/script.js
+++ aula12/ex005/script.js
@@ -8,19 +8,44 @@ function checar() {
     let anonas = document.querySelector('#ano').value;
     let container = document.querySelector('#container');
     let ilog = document.querySelector('#log');
-    let sexo = document.getElementsByTagName('sexo');
+    let sexo = document.getElementsByName('sexo');
     let idade = anoatual - anonas;
 
     container.appendChild(img);
     img.id = 'image';
 
-    if (anonas == 0 || anonas > anoatual){
+    if (anonas == 0 || anonas > anoatual) {
         ilog.innerHTML = 'Data inválida!'
+        img.style.background = '#fff none'
     } else
-        if(sexo.checked){
-            if(idade >= 0 && idade < 10){
+        if (sexo[0].checked) {
+            ilog.innerHTML = `Aqui temos um homem de ${idade} anos`
+            if (idade >= 0 && idade < 10) {
                 img.style.backgroundImage = 'url(imagens/menino.jpg)'
-            }
+            } else
+                if (idade < 21) {
+                    img.style.backgroundImage = "url(imagens/rapaz.jpg"
+                } else
+                    if (idade < 50) {
+                        img.style.backgroundImage = 'url(imagens/homem.jpg)'
+                    } else {
+                        img.style.backgroundImage = 'url(imagens/idoso.jpg)'
+                    }
+        }
+
+        if (sexo[1].checked) {
+            ilog.innerHTML = `Aqui temos uma mulher de ${idade} anos`
+            if (idade >= 0 && idade < 10) {
+                img.style.backgroundImage = 'url(imagens/menina.jpg)'
+            } else
+                if(idade < 21) {
+                    img.style.backgroundImage = 'url(imagens/moça.jpg)'
+                } else
+                    if(idade < 50) {
+                        img.style.backgroundImage = 'url(imagens/mulher.jpg)'
+                    } else{
+                        img.style.backgroundImage = 'url(imagens/idosa.jpg)'
+                    }
         }
 
 }
