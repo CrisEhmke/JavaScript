887297128b038a7fe1a09400590cd906954256ea
Felipe Cristian Ehmke <ehmke1995@gmail.com>
2022-10-10 20:37:44 -0300

    Create ex001.js

---
+++ aula13/ex001.js
@@ -0,0 +1,16 @@
+//faz a validação no início
+let c = 1
+
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//faz a validação no final
+
+//let c = 1
+
+// do {
+//     console.log(`Passo ${c}`)
+//     c++
+// } while (c <= 3)
