8df9770cc78cf2f351811f14f203e13787a3e6a2
Felipe Cristian Ehmke <ehmke1995@gmail.com>
2022-10-22 23:25:45 -0300

    create aula 15

---
+++ aula15/ex001.js
@@ -0,0 +1,10 @@
+let parImpar = (num) =>{
+    if(num%2 == 0){
+        console.log(`Par`)
+    } else {
+        console.log(`Impar`)
+    }
+}
+
+let res = parImpar(2)
+
---
+++ ex003.js
@@ -0,0 +1,9 @@
+let fatorial = (num) =>{
+    let fat = 1
+    for(let c = num; c > 1; c--){
+         fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
